15b5b0392644bce9f0770d76b9ee764fa5080808
gecuasapaz <gecuasapaz@pucesi.edu.ec>
2025-11-26 15:20:25 -0500

    test 1

---
+++ ec.com.sidesoft.closecash.indumot/src/ec/com/sidesoft/closecash/indumot/Components.java
@@ -0,0 +1,17 @@
+package ec.com.sidesoft.closecash.indumot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.openbravo.client.kernel.BaseComponentProvider;
+import org.openbravo.client.kernel.Component;
+import org.openbravo.client.kernel.ComponentProvider;
+
+public class Components {
+	public int multiplicar(int a, int b) {
+        return a * b;
+    }
+}
