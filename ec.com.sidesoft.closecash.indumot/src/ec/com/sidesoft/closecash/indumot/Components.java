6b5220f928d35aae05c0b7300c17e43a196dc630
gecuasapaz <gecuasapaz@pucesi.edu.ec>
2025-11-26 15:37:51 -0500

    Ajustes

--- ec.com.sidesoft.closecash.indumot/src/ec/com/sidesoft/closecash/indumot/Components.java
+++ ec.com.sidesoft.closecash.indumot/src/ec/com/sidesoft/closecash/indumot/Components.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.enterprise.context.ApplicationScoped;
-
-import org.openbravo.client.kernel.BaseComponentProvider;
-import org.openbravo.client.kernel.Component;
-import org.openbravo.client.kernel.ComponentProvider;
 
 public class Components {
 
