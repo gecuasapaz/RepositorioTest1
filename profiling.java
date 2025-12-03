bed673d95fb15af6303a68653700fa9fcd848e22
gecuasapaz <gecuasapaz@pucesi.edu.ec>
2025-12-03 16:23:29 -0500

    tes

--- profiling.java
+++ profiling.java
@@ -132,7 +132,7 @@ public class profiling implements WebService{
 	public static Date truncateDate(Date date) {
 	    Calendar cal = Calendar.getInstance();
 	    cal.setTime(date);
-	    cal.set(Calendar.HOUR_OF_DAY, 0);
+	    cal.set(Calendar.HOUR_OF_DAY, 0); 
 	    cal.set(Calendar.MINUTE, 0);
 	    cal.set(Calendar.SECOND, 0);
 	    cal.set(Calendar.MILLISECOND, 0);
