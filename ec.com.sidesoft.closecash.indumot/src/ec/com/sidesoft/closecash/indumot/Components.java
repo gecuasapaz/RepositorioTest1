138459113754da4e393d3c8d8eb6a1cd30c9e964
gecuasapaz <gecuasapaz@pucesi.edu.ec>
2025-12-19 13:19:13 -0500

    test

--- ec.com.sidesoft.closecash.indumot/src/ec/com/sidesoft/closecash/indumot/Components.java
+++ ec.com.sidesoft.closecash.indumot/src/ec/com/sidesoft/closecash/indumot/Components.java
@@ -24,6 +24,7 @@ public class Components {
             OBDal.getInstance().save(offerProduct);
         } finally {
             OBContext.restorePreviousMode();
+            OBContext.setAdminMode();
         }
     }
 
