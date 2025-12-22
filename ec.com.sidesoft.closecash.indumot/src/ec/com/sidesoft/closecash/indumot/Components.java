115eb8f2db0f3d354a3bef69dfa433dc3cedd5de
gecuasapaz <gecuasapaz@pucesi.edu.ec>
2025-12-22 13:16:12 -0500

    test

--- ec.com.sidesoft.closecash.indumot/src/ec/com/sidesoft/closecash/indumot/Components.java
+++ ec.com.sidesoft.closecash.indumot/src/ec/com/sidesoft/closecash/indumot/Components.java
@@ -18,7 +18,7 @@ public class Components {
     public void executeAsAdminForInternalProcess() {  
           
         try { 
-           OBContext.setAdminMode(false);  
+           OBContext.setAdminMode();  
         } catch(Exception e){ 
             throw new OBException("Error ejecutando lógica administrativa", e);
         }finally {
